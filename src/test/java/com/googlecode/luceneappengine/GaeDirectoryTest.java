081389a1f0a36f5dce6c9a8b2467b21cce7f9160
UltimaPhoenix <fabio.grucci@gmail.com>
2018-07-02 23:22:55 +0200

    add test on 7.4 lucene version

--- src/test/java/com/googlecode/luceneappengine/GaeDirectoryTest.java
+++ src/test/java/com/googlecode/luceneappengine/GaeDirectoryTest.java
@@ -52,6 +52,7 @@ public class GaeDirectoryTest extends LocalDatastoreTest {
         Version.LUCENE_7_1_0,
         Version.LUCENE_7_2_0,
         Version.LUCENE_7_3_0, Version.LUCENE_7_3_1,
+        Version.LUCENE_7_4_0,
 		Version.LUCENE_CURRENT,
 		Version.LATEST
 	};
